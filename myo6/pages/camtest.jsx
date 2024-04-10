18f07acf98cb36f93d916024c37ce2b73ffea164
QuentinFerron <quentin.ferron@edu.ece.fr>
2024-04-10 16:23:19 +0200

    cam suppr

--- myo6/pages/camtest.jsx
+++ myo6/pages/camtest.jsx
@@ -1,49 +1,49 @@
-import React, { useState, useRef } from 'react';
-import Webcam from 'react-webcam';
+// import React, { useState, useRef } from 'react';
+// import Webcam from 'react-webcam';
 
-const WebcamComponent = () => {
-  const [deviceId, setDeviceId] = useState({});
-  const webcamRef = useRef(null);
+// const WebcamComponent = () => {
+//   const [deviceId, setDeviceId] = useState({});
+//   const webcamRef = useRef(null);
 
-  const deviceIds = React.useMemo(
-    () =>
-      navigator.mediaDevices
-        .enumerateDevices()
-        .then(devices => devices.filter(device => device.kind === 'videoinput'))
-        .then(devices => devices.map(device => device.deviceId)),
-    []
-  );
+//   const deviceIds = React.useMemo(
+//     () =>
+//       navigator.mediaDevices
+//         .enumerateDevices()
+//         .then(devices => devices.filter(device => device.kind === 'videoinput'))
+//         .then(devices => devices.map(device => device.deviceId)),
+//     []
+//   );
 
-  const handleDevices = React.useCallback(
-    mediaDevices =>
-      setDeviceId(mediaDevices.map(mediaDevice => mediaDevice.deviceId)),
-    []
-  );
+//   const handleDevices = React.useCallback(
+//     mediaDevices =>
+//       setDeviceId(mediaDevices.map(mediaDevice => mediaDevice.deviceId)),
+//     []
+//   );
 
-  const handleStartCaptureClick = React.useCallback(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { deviceId: deviceId } })
-      .then(stream => {
-        const video = webcamRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(err => {
-        console.error('error:', err);
-      });
-  }, [deviceId]);
+//   const handleStartCaptureClick = React.useCallback(() => {
+//     navigator.mediaDevices
+//       .getUserMedia({ video: { deviceId: deviceId } })
+//       .then(stream => {
+//         const video = webcamRef.current;
+//         video.srcObject = stream;
+//         video.play();
+//       })
+//       .catch(err => {
+//         console.error('error:', err);
+//       });
+//   }, [deviceId]);
 
-  return (
-    <div>
-      <Webcam
-        ref={webcamRef}
-        audioConstraints={false}
-        deviceId={deviceId}
-        onUserMedia={handleDevices}
-      />
-      <button onClick={handleStartCaptureClick}>Démarrer la capture</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Webcam
+//         ref={webcamRef}
+//         audioConstraints={false}
+//         deviceId={deviceId}
+//         onUserMedia={handleDevices}
+//       />
+//       <button onClick={handleStartCaptureClick}>Démarrer la capture</button>
+//     </div>
+//   );
+// };
 
-export default WebcamComponent;
+// export default WebcamComponent;
