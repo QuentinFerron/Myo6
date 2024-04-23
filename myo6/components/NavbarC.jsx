b13efef965d9b5b71f44b370ec87ac792d306c65
QuentinFerron <quentin.ferron@edu.ece.fr>
2024-04-23 10:13:58 +0200

    a

--- myo6/components/NavbarC.jsx
+++ myo6/components/NavbarC.jsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import SideBar from "./SideBar";
 import Image from "next/image";
 import Link from "next/link";
-import Image from "next/image";
 import { useEffect } from 'react'
 import { useState } from 'react'
 
